8db6b15701d69992a0f7a38e520e7018d5981a55
oh-min <ohyemin96@gmail.com>
2023-09-07 13:43:38 +1000

    230907

--- src/split.java
+++ src/split.java
@@ -4,17 +4,27 @@ public class split {
 	public static void main(String[] args) {
 		String str = "aaa-bbb-ccc";
 
-		// str을 -를 기준으로 분리하여 저장할 str_split 배열 생성
-		String[] str_split = str.split("-");
+		/* split(String regex) */
+		// - 를 기준으로 분리하여 저장하는 배열 생성
+		String[] str_split = str.split("=");
 
-		String split_result01 = str_split[0];
-		String split_result02 = str_split[1];
-		String split_result03 = str_split[2];
+		String split_result_01 = str_split[0];
+		String split_result_02 = str_split[1];
+		String split_result_03 = str_split[2];
 
-		System.out.println(split_result01);
-		System.out.println(split_result02);
-		System.out.println(split_result03);
+		System.out.println(split_result_01);	// aaa
+		System.out.println(split_result_02);	// bbb
+		System.out.println(split_result_03);	// ccc
 
+		/* split(String regex,int limit) */
+		// - 를 기준으로 분리하고, 배열의 길이가 2인 배열 생성
+//		String[] str_split_02 = str.split("-", 2);
+//
+//		String split_result_01 = str_split_02[0];
+//		String split_result_02 = str_split_02[1];
+//
+//		System.out.println(split_result_01); // aaa
+//		System.out.println(split_result_02); // bbb-ccc
 	}
 
 }
